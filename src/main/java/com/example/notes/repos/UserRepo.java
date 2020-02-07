fbd9199c121675098cee4be42218a8f3f7ecafbd
Artem <tema_l@mail.ru>
2020-02-08 02:14:16 +0300

    Initial commit

---
+++ src/main/java/com/example/notes/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.notes.repos;
+
+import com.example.notes.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
